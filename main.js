9eeaf8dd3c8a2a5af044e67951760aff1b3ed5ae
flapmfy <frantis3k.kasik@gmail.com>
2023-09-16 21:13:54 +0200

    Finish site

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 const menuOpen = document.querySelector('.menu-open');
 const menuClose = document.querySelector('.menu-close');
 const nav = document.querySelector('.primary-nav');
-const overlay = document.querySelector('.overlay');
+const header = document.querySelector('.site-header');
+const scrollWatcher = document.querySelector('.scroll-watcher');
+
+const scrollObserver = new IntersectionObserver((entries) => {
+  header.classList.toggle('scrolled', !entries[0].isIntersecting);
+});
+
+function closeNav() {
+  nav.setAttribute('aria-expanded', false);
+}
+
+function openNav() {
+  nav.setAttribute('aria-expanded', true);
+}
 
 menuOpen.addEventListener('click', () => {
-  nav.classList.add('active');
-  overlay.classList.add('active');
+  openNav();
 });
 
 menuClose.addEventListener('click', () => {
-  nav.classList.remove('active');
-  overlay.classList.remove('active');
+  closeNav();
 });
+
+scrollObserver.observe(scrollWatcher);
